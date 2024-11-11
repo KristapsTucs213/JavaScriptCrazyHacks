f7a153722fcd3919d68575dbc9f3413da965cd4e
Andrejs Nikolajevs <you@example.com>
2024-11-11 13:53:26 +0200

    4 pabeigts

--- asd.js
+++ asd.js
@@ -1,10 +1,37 @@
-console.log("Kristaps");
-const vertiba = document.getElementById("text");
-const vertibaInput = vertiba.value;
-console.log(vertibaInput);
+// console.log("Kristaps");
+// const vertiba = document.getElementById("text");
+// const vertibaInput = vertiba.value;
+// console.log(vertibaInput);
+// const button = document.getElementById("submiterer")
 
-function clicker(){
-    console.log("Vards")
+
+// function printName(){
+//     console.log("Vards")
+// }
+// button.addEventListener("click", printName);
+
+
+const vertiba = document.getElementById("skaitlis");
+const vertiba2 = document.getElementById("skaitlitis");
+const button = document.getElementById("pog")
+let intins = 0;
+
+function parbauditajs(){
+    let returnText = "Skaitli: ";
+    const parag = document.getElementById("paragrah")
+    if(vertiba.value>vertiba2.value){
+        intins = 1;
+    }else{
+        alert("NESTRADAAAAAAAAAAAAAAAAAAAAAAAA")
+    }
+    if(intins = 1){
+        for (let i = vertiba2.value; i <= vertiba.value; i++) {
+            // console.log(i);
+            returnText = returnText + i + ", "
+            // const parag = document.getElementById("paragrah").innerHTML = i;
+            parag.innerHTML = returnText;
+        }
+
+    }
 }
-
-element.addEventListener("click", clicker);
+button.addEventListener("click", parbauditajs);
